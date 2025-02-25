7482e523bf9f2b40d2d72980656f557a5dbf3541
Alexander Waya Nguku <alex.waya@gmail.com>
2025-02-25 14:29:30 +0300

    Created the index view that allows users to type in the chat room they want to join

---
+++ config/asgi.py
@@ -0,0 +1,33 @@
+"""
+ASGI config for config project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# application = get_asgi_application()
+
+
+
+
+import os
+
+import django
+from channels.http import AsgiHandler
+from channels.routing import ProtocolTypeRouter
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+application = ProtocolTypeRouter({
+  "http": AsgiHandler(),
+  # We will add WebSocket protocol later, but for now it's just HTTP.
+})
